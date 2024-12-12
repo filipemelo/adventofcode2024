32f543fe69c19c904467771c9c0a7f298962f803
filipemelo <filipemelo.br@gmail.com>
2024-12-12 05:42:59 -0300

    Improving organization.

--- days/02/main.go
+++ days/02/main.go
@@ -16,39 +16,69 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 	safe := 0
+	safeProblemDampener := 0
 	for scanner.Scan() {
 		line := scanner.Text()
 
 		parts := strings.Fields(line)
+		rowNumbers := getRowNumbersFromParts(parts)
 
-		unsafe := false
-		allDecreasing := (internal.ReadInt(parts[1]) < internal.ReadInt(parts[0]))
-		allIncreasing := !allDecreasing
-		for i := 1; i < len(parts); i++ {
-			previousLevel := internal.ReadInt(parts[i-1])
-			currentLevel := internal.ReadInt(parts[i])
-			abs := internal.AbsDiff(previousLevel, currentLevel)
-
-			if abs < 1 || abs > 3 {
-				unsafe = true
-				break
-			}
-
-			if allDecreasing && (currentLevel > previousLevel) {
-				unsafe = true
-				break
-			}
-
-			if allIncreasing && (currentLevel < previousLevel) {
-				unsafe = true
-				break
-			}
-		}
-
-		if !unsafe {
+		if !firstPartUnsafe(rowNumbers) {
 			safe += 1
+		} else {
+			if !secondPartUnsafe(rowNumbers) {
+				safeProblemDampener += 1
+			}
 		}
 	}
 
 	fmt.Println("Safe reports:", safe)
+	fmt.Println("Safe reports problem dampener:", (safe + safeProblemDampener))
+}
+
+func firstPartUnsafe(rowNumbers []int) bool {
+	isDecreasing := true
+	isIncreasing := true
+
+	for i := 1; i < len(rowNumbers); i++ {
+		previousLevel := rowNumbers[i-1]
+		currentLevel := rowNumbers[i]
+		abs := internal.AbsDiff(previousLevel, currentLevel)
+
+		if abs < 1 || abs > 3 {
+			return true
+		}
+
+		if currentLevel > previousLevel {
+			isDecreasing = false
+		}
+		if currentLevel < previousLevel {
+			isIncreasing = false
+		}
+	}
+
+	return !(isIncreasing || isDecreasing)
+}
+
+func getRowNumbersFromParts(parts []string) []int {
+	var row []int
+	for i := 0; i < len(parts); i++ {
+		row = append(row, internal.ReadInt(parts[i]))
+	}
+	return row
+}
+
+func secondPartUnsafe(rowNumbers []int) bool {
+	for i := 0; i < len(rowNumbers); i++ {
+		modifiedRow := removeElement(rowNumbers, i)
+
+		if !firstPartUnsafe(modifiedRow) {
+			return false
+		}
+	}
+	return true
+}
+
+func removeElement(slice []int, index int) []int {
+	return append(slice[:index], slice[index+1:]...)
 }
